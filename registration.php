342d3faa50846f8e5c4f125a8c41ced217a30d8f
Noblepal <zeronillzero@gmail.com>
2022-04-04 08:51:27 +0300

    Replace placeholders

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    ':module-namespace_:module-name',
+    'RewardsBag_Rewards',
     __DIR__ . '/src'
 );
